eea71ba1c1cf3f368f0f2b73de7cb3df5c4dd85d
mmpdinis <mmp.dinis@gmail.com>
2023-06-05 15:19:36 +0100

    correcoes

--- analysis.py
+++ analysis.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 from tkinter.filedialog import askdirectory
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import savgol_filter
 
 folder_path = askdirectory(title = "Select Folder")  # Replace with the path to your folder
 
@@ -11,10 +13,13 @@ print(file_list)
 # Filter the list to only include Excel files
 excel_files = [file for file in file_list if file.endswith('.csv')]
 
-
 Tensor = {}
-
-
+Features = {}
+blocks = ["A","B","D","N","P","PP","K","L"]
+for a in blocks:
+    Tensor[a] = []
+    Features[a] = []
+print(Tensor)
 # Iterate over each Excel file
 for file in excel_files:
     file_path = os.path.join(folder_path, file)  # Create the full file path
@@ -33,8 +38,15 @@ for file in excel_files:
                 Tensor[current_class] += [temp_block]
                 temp_block=np.empty((0,5))
         else:
-            #print("bloco feito  ", current_class)
-            #print(Tensor)
+            mean = np.mean(temp_block[:,3])
+            hiv = np.std(temp_block[:,3])
+            rms = np.sqrt(np.mean(temp_block[:,3]**2))
+            
+            fft_result = np.abs(np.fft.fft(temp_block[:,3]))
+            freq = np.fft.fftfreq(len(temp_block[:,3]),1/1000)
+
+            Features[current_class] += [[mean,hiv,rms,freq,fft_result]]
+
             if current_class not in Tensor:
                 Tensor[current_class] = [temp_block]
             else:
@@ -43,7 +55,10 @@ for file in excel_files:
             temp_block=np.empty((0,5))
             current_class = str(df.loc[i, 'Class'])
             
-
-        
+print(Features["K"][0][4])
+print(Features["K"][0][3])
+smoothed_data = savgol_filter(Features["K"][0][4][:round(len(Features["K"][0][3])/2)], 20, 2)
+plt.plot(Features["K"][0][3][:round(len(Features["K"][0][3])/2)],smoothed_data)
+plt.show()        
         
         
--- filtroPB.py
+++ filtroPB.py
@@ -24,33 +24,11 @@ if __name__ == "__main__":
     lowcut = 80.0
     highcut = 499
 
-
-    plt.figure(1)
-    plt.clf()
-    for order in [3, 6, 9]:
-        b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-        w, h = freqz(b, a, fs=fs, worN=2000)
-        plt.plot(w, abs(h), label="order = %d" % order)
-
-    plt.plot([0, 0.5 * fs], [np.sqrt(0.5), np.sqrt(0.5)],
-             '--', label='sqrt(0.5)')
-    plt.xlabel('Frequency (Hz)')
-    plt.ylabel('Gain')
-    plt.grid(True)
-    plt.legend(loc='best')
-
-
     # Filter a noisy signal.
-    T = 91
-    nsamples = T * fs
+    x =  pandas.read_csv(filedialog.askopenfilename())["ACCraw"]
+    nsamples = len(x)
     t = np.arange(0, nsamples) / fs
-    a = 0.02
-    f0 = 600.0
-    x =  pandas.read_csv(filedialog.askopenfilename())["EMGraw"]
-
     y = butter_bandpass_filter(x, lowcut, highcut, fs, order=6)
-
-
 
     path= askdirectory(title= "Select Folder")
     name=input("File name?: \n")
@@ -60,23 +38,8 @@ if __name__ == "__main__":
         i+=1
     name+=".csv"
     file    = open(path+"/"+name, "a", newline="")
-    csv.writer(file).writerow(["Nsample","Timestamp", "EMGraw", "EMGfilter"])
+    csv.writer(file).writerow(["Nsample","Timestamp", "ACCraw", "EMGfilter"])
     writer=csv.writer(file)
 
     for index in range(len(x)):
         writer.writerow([index, index/fs, x[index], y[index]])
-
-
-    plt.figure(2)
-    plt.clf()
-    plt.plot(t, x, label='Noisy signal')
-
-
-    plt.plot(t, y, label='Filtered signal (%g Hz)' % f0)
-    plt.xlabel('time (seconds)')
-    plt.hlines([-a, a], 0, T, linestyles='--')
-    plt.grid(True)
-    plt.axis('tight')
-    plt.legend(loc='upper left')
-
-    plt.show()
