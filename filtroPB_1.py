9b61ad92ee2c459072d5183548399480a070eb99
mmpdinis <mmp.dinis@gmail.com>
2023-06-05 18:19:42 +0100

    sklearn

--- filtroPB_1.py
+++ filtroPB_1.py
@@ -4,6 +4,8 @@ from tkinter import filedialog
 import csv
 import os 
 from tkinter.filedialog import askdirectory
+import numpy as np
+from sklearn.decomposition import FastICA
 
 def butter_bandpass(lowcut, highcut, fs, order=5):
     return butter(order, [lowcut, highcut], fs=fs, btype='band')
@@ -12,6 +14,20 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
     b, a = butter_bandpass(lowcut, highcut, fs, order=order)
     y = lfilter(b, a, data)
     return y
+
+def remove_ecg_using_ica(emg_signal):
+    # Apply ICA to separate independent components
+    ica = FastICA(n_components=2)  # Assuming ECG is one of the two components
+    components = ica.fit_transform(emg_signal.reshape(-1, 1))
+
+    # Identify ECG component based on correlation with the original signal
+    correlations = np.corrcoef(emg_signal, components[:, 0])
+    ecg_component = components[:, np.argmax(correlations[0, :])]
+
+    # Remove ECG component from the original signal
+    filtered_emg = emg_signal - ecg_component
+
+    return filtered_emg
 
 
 if __name__ == "__main__":
@@ -30,6 +46,8 @@ if __name__ == "__main__":
         nsamples = len(x)
         t = np.arange(0, nsamples) / fs
         y = butter_bandpass_filter(x, lowcut, highcut, fs, order=6)
+        filtered_emg = remove_ecg_using_ica(y)
+
 
         path= askdirectory(title= "Select Folder")
         name=input("File name?: \n")
@@ -43,4 +61,6 @@ if __name__ == "__main__":
         writer=csv.writer(file)
 
         for index in range(len(x)):
-            writer.writerow([index, index/fs, x[index], y[index]])
+            writer.writerow([index, index/fs, x[index], filtered_emg[index]])
+
+
